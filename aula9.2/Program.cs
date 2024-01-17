cc5b56e5c491f6c408bff698e796952473776ebe
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-17 14:13:24 +0000

    CFB9.2

---
+++ aula9.2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace aula9._2
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int num = 10;
+
+            num = num << 3;
+
+            Console.WriteLine(num); 
+        }
+    }
+}
